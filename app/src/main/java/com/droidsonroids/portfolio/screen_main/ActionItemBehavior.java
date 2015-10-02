5b39989ab956a99afd5fd768a5f978d0f4c6023a
mariusz <mariusz.brona@droidsonroids.pl>
2015-10-02 14:12:35 +0200

    Ver 1.0 Release!

--- app/src/main/java/com/droidsonroids/portfolio/screen_main/ActionItemBehavior.java
+++ app/src/main/java/com/droidsonroids/portfolio/screen_main/ActionItemBehavior.java
@@ -10,74 +10,75 @@ import com.droidsonroids.portfolio.R;
 
 public class ActionItemBehavior extends CoordinatorLayout.Behavior<ImageView> {
 
-private final static float MIN_AVATAR_PERCENTAGE_SIZE = 0.3f;
-private final static int EXTRA_FINAL_AVATAR_PADDING = 80;
+	private final static float MIN_AVATAR_PERCENTAGE_SIZE = 0.3f;
+	private final static int EXTRA_FINAL_AVATAR_PADDING = 80;
 
-private final static String TAG = "behavior";
-private final Context mContext;
-private float mImageMaxSize;
-private float mMarginTop;
+	private final static String TAG = "behavior";
+	private final Context mContext;
+	private float mImageMaxSize;
+	private float mMarginTop;
 
-public ActionItemBehavior(Context context, AttributeSet attrs) {
-	mContext = context;
-	init();
+	public ActionItemBehavior(Context context, AttributeSet attrs) {
+		mContext = context;
+		init();
 	}
 
-private void init() {
-	mImageMaxSize = mContext.getResources().getDimensionPixelSize(R.dimen.toolbar_circle_image_height);
-	mMarginTop = mContext.getResources().getDimension(R.dimen.image_margin);
+	private void init() {
+		mImageMaxSize =
+			mContext.getResources().getDimensionPixelSize(R.dimen.toolbar_circle_image_height);
+		mMarginTop = mContext.getResources().getDimension(R.dimen.image_margin);
 	}
 
-@Override public boolean layoutDependsOn(CoordinatorLayout parent, ImageView child,
-	View dependency) {
-	return dependency instanceof Toolbar;
+	@Override
+	public boolean layoutDependsOn(CoordinatorLayout parent, ImageView child, View dependency) {
+		return dependency instanceof Toolbar;
 	}
 
-@Override public boolean onMeasureChild(CoordinatorLayout parent, ImageView child,
-	int parentWidthMeasureSpec, int widthUsed, int parentHeightMeasureSpec, int heightUsed) {
-	return super.onMeasureChild(parent, child, parentWidthMeasureSpec, widthUsed,
-	parentHeightMeasureSpec, heightUsed);
+	@Override
+	public boolean onMeasureChild(CoordinatorLayout parent, ImageView child,
+		int parentWidthMeasureSpec, int widthUsed, int parentHeightMeasureSpec, int heightUsed) {
+		return super.onMeasureChild(parent, child, parentWidthMeasureSpec, widthUsed,
+			parentHeightMeasureSpec, heightUsed);
 	}
 
-@Override public boolean onDependentViewChanged(CoordinatorLayout parent, ImageView child,
-	View dependency) {
+	@Override
+	public boolean onDependentViewChanged(CoordinatorLayout parent, ImageView child,
+		View dependency) {
 
-	CoordinatorLayout.LayoutParams lp =
-	(CoordinatorLayout.LayoutParams) child.getLayoutParams();
+		CoordinatorLayout.LayoutParams lp =
+			(CoordinatorLayout.LayoutParams) child.getLayoutParams();
 
-final int maxNumber = (int) (mMarginTop - getStatusBarHeight());
+		final int maxNumber = (int) (mMarginTop - getStatusBarHeight());
 
-	float percentageFactor = dependency.getY() / maxNumber;
-	int proportionalAvatarSize = (int) (mImageMaxSize * (percentageFactor));
+		float percentageFactor = dependency.getY() / maxNumber;
+		int proportionalAvatarSize = (int) (mImageMaxSize * (percentageFactor));
 
-	float childMarginTop = dependency.getY() - (child.getHeight() / 2);
-	float childMarginLeft = (dependency.getWidth() / 2) - (child.getWidth() / 2);
-	float pChildMarginLeft = childMarginLeft * percentageFactor;
+		float childMarginTop = dependency.getY() - (child.getHeight() / 2);
+		float childMarginLeft = (dependency.getWidth() / 2) - (child.getWidth() / 2);
+		float pChildMarginLeft = childMarginLeft * percentageFactor;
 
-	int extraFinalPadding = (int) (EXTRA_FINAL_AVATAR_PADDING * (1f - percentageFactor));
+		int extraFinalPadding = (int) (EXTRA_FINAL_AVATAR_PADDING * (1f - percentageFactor));
 
-	if (percentageFactor >= MIN_AVATAR_PERCENTAGE_SIZE) {
-	lp.width = proportionalAvatarSize;
-	lp.height = proportionalAvatarSize;
+		if (percentageFactor >= MIN_AVATAR_PERCENTAGE_SIZE) {
+			lp.width = proportionalAvatarSize;
+			lp.height = proportionalAvatarSize;
+		}
+
+		lp.setMargins(lp.leftMargin, (int) childMarginTop + extraFinalPadding,
+			(int) pChildMarginLeft + extraFinalPadding, lp.bottomMargin);
+
+		child.setLayoutParams(lp);
+		return true;
 	}
 
-	lp.setMargins(lp.leftMargin,
-	(int) childMarginTop + extraFinalPadding, (int) pChildMarginLeft + extraFinalPadding, lp.bottomMargin);
+	public int getStatusBarHeight() {
+		int result = 0;
+		int resourceId =
+			mContext.getResources().getIdentifier("status_bar_height", "dimen", "android");
 
-	child.setLayoutParams(lp);
-	return true;
+		if (resourceId > 0) {
+			result = mContext.getResources().getDimensionPixelSize(resourceId);
+		}
+		return result;
 	}
-
-public int getStatusBarHeight() {
-	int result = 0;
-	int resourceId =
-	mContext.getResources().getIdentifier("status_bar_height", "dimen", "android");
-
-	if (resourceId > 0) {
-	result = mContext.getResources().getDimensionPixelSize(resourceId);
-	}
-	return result;
-	}
-
-
-	}
+}
--- app/src/main/java/com/droidsonroids/portfolio/screen_main/main/MainActivity.java
+++ app/src/main/java/com/droidsonroids/portfolio/screen_main/main/MainActivity.java
@@ -1,6 +1,6 @@
 package com.droidsonroids.portfolio.screen_main.main;
 
-import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
@@ -8,38 +8,45 @@ import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.TextSwitcher;
 import android.widget.TextView;
-
+import butterknife.Bind;
+import butterknife.OnClick;
 import com.droidsonroids.portfolio.R;
 import com.droidsonroids.portfolio.mvp.MvpActivity;
 import com.droidsonroids.portfolio.screen_details.AppDetailsActivity;
 import com.droidsonroids.portfolio.screen_main.cards.AppItem;
 import com.droidsonroids.portfolio.screen_main.cards.Apps;
 import com.droidsonroids.portfolio.screen_main.cards.AppsRecyclerAdapter;
+import de.hdodenhof.circleimageview.CircleImageView;
 
-import butterknife.Bind;
+public class MainActivity extends MvpActivity<MainActivityView, MainActivityPresenter>
+	implements MainActivityView, AppBarLayout.OnOffsetChangedListener {
+	private static final float PERCENTAGE_TO_HIDE_TITLE_DETAILS = 0.2f;
+	private static final int COLLAPSING_ANIMATIONS_DURATION = 300;
 
-public class MainActivity extends MvpActivity<MainActivityView, MainActivityPresenter> implements MainActivityView, AppBarLayout.OnOffsetChangedListener {
-	private static final float PERCENTAGE_TO_SHOW_TITLE_AT_TOOLBAR = 0.9f;
-	private static final float PERCENTAGE_TO_HIDE_TITLE_DETAILS = 0.3f;
-	private static final int ALPHA_ANIMATIONS_DURATION = 200;
-
-	private boolean mIsTheTitleVisible = false;
 	private boolean mIsTheTitleContainerVisible = true;
-
 
 	@Bind(R.id.toolbar) Toolbar mToolbar;
 	@Bind(R.id.activity_main_ll_title_container) LinearLayout mLlTitleContainer;
-	@Bind(R.id.activity_main_ll_action_items_container) LinearLayout mLlActionItemsContainer;
 	@Bind(R.id.activity_main_tv_toolbar_title) TextView mTvToolbarTitle;
+	@Bind(R.id.activity_main_tv_name) TextSwitcher mTsName;
+	@Bind(R.id.activity_main_tv_occupation) TextSwitcher mTsOccupation;
+	@Bind(R.id.activity_main_tv_working_at) TextSwitcher mTsWorkingAt;
 	@Bind(R.id.activity_main_app_bar_layout) AppBarLayout mAppBarLayout;
 	@Bind(R.id.activity_main_rl_placeholder) RelativeLayout mRlPlaceholder;
 	@Bind(R.id.activity_main_fl_title) FrameLayout mFlTitleContainer;
 	@Bind(R.id.toolbar_rl) RelativeLayout mRlToolbarMainLayout;
+	@Bind(R.id.activity_main_civ_mariusz) CircleImageView mCivMariusz;
+	@Bind(R.id.activity_main_civ_paulina) CircleImageView mCivPaulina;
 	@Bind(R.id.activity_main_cl) CoordinatorLayout mClCoordinatorLayout;
 	@Bind(R.id.cards_recyclerView) RecyclerView mCardsRecyclerView;
 
@@ -55,7 +62,41 @@ public class MainActivity extends MvpActivity<MainActivityView, MainActivityPres
 
 	@Override
 	public void onOffsetChanged(AppBarLayout appBarLayout, int i) {
+		int maxScroll = appBarLayout.getTotalScrollRange();
+		float percentage = (float) Math.abs(i) / (float) maxScroll;
+		handleViewCollapsing(percentage);
+	}
 
+	private void handleViewCollapsing(float percentage) {
+		if(percentage >= PERCENTAGE_TO_HIDE_TITLE_DETAILS) {
+			if(mIsTheTitleContainerVisible) {
+				startScaleAnimationDown(mCivMariusz, mCivPaulina);
+				mIsTheTitleContainerVisible = false;
+			}
+		} else {
+			if(!mIsTheTitleContainerVisible) {
+				startScaleAnimationUp(mCivMariusz, mCivPaulina);
+				mIsTheTitleContainerVisible = true;
+			}
+		}
+	}
+
+	private void startScaleAnimationDown(CircleImageView... civs) {
+		ScaleAnimation scaleAnimation = new ScaleAnimation(1f, 0f, 1f, 0f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+		scaleAnimation.setDuration(COLLAPSING_ANIMATIONS_DURATION);
+		for(CircleImageView civ : civs) {
+			civ.startAnimation(scaleAnimation);
+			civ.setVisibility(View.INVISIBLE);
+		}
+	}
+
+	private void startScaleAnimationUp(CircleImageView... civs) {
+		ScaleAnimation scaleAnimation = new ScaleAnimation(0f, 1f, 0f, 1f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+		scaleAnimation.setDuration(COLLAPSING_ANIMATIONS_DURATION);
+		for(CircleImageView civ : civs) {
+			civ.setVisibility(View.VISIBLE);
+			civ.startAnimation(scaleAnimation);
+		}
 	}
 
 	@Override
@@ -69,10 +110,57 @@ public class MainActivity extends MvpActivity<MainActivityView, MainActivityPres
 		mAppBarLayout.addOnOffsetChangedListener(this);
 		mClCoordinatorLayout.setOnTouchListener((v, event) -> true);
 		initParallaxValues();
+		Typeface typeface = Typeface.createFromAsset(getAssets(),"fonts/Roboto-Thin.ttf");
+		initTextSwitchers(typeface);
+	}
+
+	private void initTextSwitchers(Typeface typeface) {
+		mTsName.setInAnimation(this, android.R.anim.fade_in);
+		mTsName.setOutAnimation(this, android.R.anim.fade_out);
+		mTsName.setFactory(() -> {
+			TextView textView = new TextView(MainActivity.this);
+			textView.setGravity(Gravity.CENTER);
+			textView.setTextSize(TypedValue.COMPLEX_UNIT_PX,
+				getResources().getDimension(R.dimen.name_text_size));
+			textView.setTypeface(typeface);
+			textView.setTextColor(getResources().getColor(R.color.text_icon_color));
+			return textView;
+		});
+
+		mTsName.setText("Droids on Roids");
+
+		mTsOccupation.setInAnimation(this, android.R.anim.fade_in);
+		mTsOccupation.setOutAnimation(this, android.R.anim.fade_out);
+		mTsOccupation.setFactory(() -> {
+			TextView textView = new TextView(MainActivity.this);
+			textView.setGravity(Gravity.CENTER);
+			textView.setTextSize(TypedValue.COMPLEX_UNIT_PX,
+				getResources().getDimension(R.dimen.occupation_text_size));
+			textView.setTypeface(typeface);
+			textView.setTextColor(getResources().getColor(R.color.text_icon_color));
+			return textView;
+		});
+
+		mTsOccupation.setText("app development company");
+
+		mTsWorkingAt.setInAnimation(this, android.R.anim.fade_in);
+		mTsWorkingAt.setOutAnimation(this, android.R.anim.fade_out);
+		mTsWorkingAt.setFactory(() -> {
+			TextView textView = new TextView(MainActivity.this);
+			textView.setGravity(Gravity.CENTER);
+			textView.setTypeface(typeface);
+			textView.setTextSize(TypedValue.COMPLEX_UNIT_PX,
+				getResources().getDimension(R.dimen.working_at_text_size));
+			textView.setTextColor(getResources().getColor(R.color.light_primary_color));
+			return textView;
+		});
+
+		mTsWorkingAt.setText("visit us @ thedroidsonroids.com");
 	}
 
 	private void initRecyclerView() {
-		AppsRecyclerAdapter adapter = new AppsRecyclerAdapter(Apps.getApps(), this::showNewActivity);
+		AppsRecyclerAdapter adapter =
+			new AppsRecyclerAdapter(Apps.getApps(), this::showNewActivity);
 		mCardsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 		mCardsRecyclerView.setHasFixedSize(true);
 		mCardsRecyclerView.setAdapter(adapter);
@@ -81,7 +169,6 @@ public class MainActivity extends MvpActivity<MainActivityView, MainActivityPres
 	private void showNewActivity(final AppItem item) {
 		AppDetailsActivity.newInstance(this, item);
 	}
-
 
 	private void initParallaxValues() {
 		CollapsingToolbarLayout.LayoutParams petDetailsLp =
@@ -94,4 +181,24 @@ public class MainActivity extends MvpActivity<MainActivityView, MainActivityPres
 		mFlTitleContainer.setLayoutParams(petBackgroundLp);
 	}
 
+	@OnClick(R.id.activity_main_civ_mariusz)
+	public void onMariuszClicked() {
+		mTsName.setText("Mariusz Brona");
+		mTsOccupation.setText("android developer");
+		mTsWorkingAt.setText("mariusz.brona@droidsonroids.pl");
+	}
+
+	@OnClick(R.id.activity_main_civ_paulina)
+	public void onPaulinaClicked() {
+		mTsName.setText("Paulina Szklarska");
+		mTsOccupation.setText("android developer");
+		mTsWorkingAt.setText("paulina.szklarska@droidsonroids.pl");
+	}
+
+	@OnClick(R.id.activity_main_rl_placeholder)
+	public void onLogoClicked() {
+		mTsName.setText("Droids on Roids");
+		mTsOccupation.setText("app development company");
+		mTsWorkingAt.setText("visit us @ thedroidsonroids.com");
+	}
 }
